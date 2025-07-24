392585bd697746beff3557eb263aff95b4c901c1
jaysonstn <jaysonstn@gmail.com>
2025-07-24 14:35:11 -0300

    Alteração pagina espera-digital

--- app_menu_reserva/src/app/site_online/pg_espera_digital/page.tsx
+++ app_menu_reserva/src/app/site_online/pg_espera_digital/page.tsx
@@ -6,8 +6,16 @@ export default function PaginaEsperaDigital() {
         <div>
             {/* Cabeçalho padrão para as páginas do site online */}
             <Header />
-            <CorpoPagina className="bg-green-500">
-                <h1>Teste</h1>
+            <CorpoPagina className="bg-[url(/imagens/cafe_manha_escuro.jpg)]  brightness-20 min-h-screen flex justify-center items-center bg-no-repeat bg-cover bg-center">
+                <div className="backdrop-blur-xs text-center border border-red-500 w-100 md:w-250 h-100 flex borde">
+                   <form className="flex flex-col">
+                   <label className="text-white">Nome Completo</label>
+                   <input className="border border-[#E2CCA6]/60 rounded-md text-white outline-none px-2 py-1" type="text" name="inputNomeCompleto" placeholder="Digite seu nome completo..." required />
+                   <label className="text-white">Nome Completo</label>
+                   <input className="border border-[#E2CCA6]/60 rounded-md text-white outline-none px-2 py-1" type="text" name="inputNomeCompleto" placeholder="Digite seu nome completo..." required />
+                   </form>
+
+                </div>
             </CorpoPagina>
         </div>
     )
