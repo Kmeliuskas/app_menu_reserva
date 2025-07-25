83b24602484c38546c22403d4255cb29efd2d77d
jaysonstn <jaysonstn@gmail.com>
2025-07-25 13:18:18 -0300

    Alteração background

--- app_menu_reserva/src/app/site_online/pg_espera_digital/page.tsx
+++ app_menu_reserva/src/app/site_online/pg_espera_digital/page.tsx
@@ -8,7 +8,7 @@ export default function PaginaEsperaDigital() {
             <Header />
             <CorpoPagina className="relative bg-[url(/imagens/cafe_manha_escuro_1.png)] min-h-screen flex justify-center items-center bg-no-repeat bg-cover bg-top">
                 <div className="absolute inset-0 bg-[#2a2a22]/40 z-0"></div> {/* Overlay com #2a2a22 */}
-                <div className="backdrop-blur-xs text-center border border-red-500 w-100 md:w-250 h-100 flex borde">
+                <div className="backdrop-sepia text-center shadow-md shadow-black/40 rounded-2xl w-100 md:w-250 h-100 flex borde">
                    <form className="flex flex-col">
                    <label className="text-white">Nome Completo</label>
                    <input className="border border-[#E2CCA6]/60 rounded-md text-white outline-none px-2 py-1" type="text" name="inputNomeCompleto" placeholder="Digite seu nome completo..." required />
